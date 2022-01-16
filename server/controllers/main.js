537c0c97e2940d596c0578bb339884fe11e99d4e
novozhilovsergeydisk <novozhilovsergeydisk@yandex.ru>
2022-01-17 00:50:18 +0300

    modify

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -1,12 +1,26 @@
 const path = require('path');
 const fs = require('fs');
-const { DTOFactory, log } = require('../helpers.js');
+const { Client, Pool } = require('pg');
+const { DTOFactory, log, generateToken, hash } = require('../helpers.js');
 const { VIEWS_PATH, STATIC_PATH } = require('../../constants.js');
 const nunjucks = require('nunjucks');
 const userService = require('../service/user-service.js');
+const conf = require('../conf.js');
 // const { parse } = require('querystring');
 
-// log({ VIEWS_PATH });
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    host: conf.mailer.host,
+    port: conf.mailer.port,
+    secure: conf.mailer.secure,
+    auth: {
+        user: conf.mailer.auth.user,
+        pass: conf.mailer.auth.pass
+    }
+});
+
+log(generateToken());
 
 nunjucks.configure(VIEWS_PATH, { autoescape: true });
 
@@ -26,6 +40,14 @@ let patients = [
     }
 ];
 
+// const pool = new Pool({
+//     user: "postgres",
+//     host: "localhosst",
+//     database: "salon_groom",
+//     password: "postgres@12345",
+//     port: 5432
+// })
+
 // Handlers
 class mainControllers {
     async order(client) {
@@ -41,16 +63,128 @@ class mainControllers {
             return item.split('=');
         });
 
-        const newObj = {};
+        const formData = {};
 
         bodyArr.forEach(item => {
             const newArr = item.split('=');
-            newObj[newArr[0]] = newArr[1];
+            formData[newArr[0]] = newArr[1];
         });
 
+        log( formData.phone );
+
+        const client_pg = new Client({
+            user: "postgres",
+            host: "localhost",
+            database: "salon_groom",
+            password: "postgres@12345",
+            port: 5432
+        });
+        await client_pg.connect();
+
+        // 1 select * from crm.clients where id = 1;
+
+        const par = formData.phone + '';
+
+        log(typeof par);
+        log(typeof '+7(916)346-5407');
+
+        const query = {
+            // give the query a unique name
+            name: 'fetch-crm.clients',
+            text: 'SELECT * FROM crm.clients WHERE phone = $1::text',
+            values: [par]
+        }
+
+        // const textSelect = "SELECT * FROM crm.clients WHERE phone = '" + par + "'";
+        //
+        // log({ textSelect });
+
+        const valuesSelect = [par];
+// callback
+//         client_pg.query(query, (err, res) => {
+//             if (err) {
+//                 console.log(err.stack)
+//             } else {
+//                 console.log(res.rows[0])
+//             }
+//         });
+// promise
+        client_pg
+            .query(query)
+            .then(res => console.log({ 'rows': res.rows[0] }))
+            .catch(e => console.error(e.stack));
+
+        // END 1
 
 
-        log({ newObj });
+        // const text = 'INSERT INTO users(name, email) VALUES($1, $2) RETURNING *'
+        // const values = ['brianc', 'brian.m.carlson@gmail.com']
+
+        const text = 'insert into crm.clients values(nextval(\'crm.clients_id_seq\'), $1, $2, $3) RETURNING *';
+        const values = [formData.name, formData.phone, null];
+// callback
+//         client_pg.query(text, values, (err, res) => {
+//             if (err) {
+//                 console.log('ERROR');
+//                 // console.log(err.stack);
+//             } else {
+//                 console.log(res.rows[0]);
+//                 // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
+//             }
+//         });
+// promise
+//         client_pg
+//             .query(text, values)
+//             .then(res => {
+//                 console.log(res.rows[0])
+//                 // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
+//             })
+//             .catch(e => console.error(e.stack))
+// async/await
+        try {
+            const res = await client_pg.query(text, values)
+            const data = res.rows[0];
+
+            if (data) {
+                const mailOptions = {
+                    from: conf.mailOptions.from,
+                    to: conf.mailOptions.to,
+                    subject: conf.mailOptions.subject,
+                    text: 'Клиент: ' + data.name + ' -- ' + data.phone
+                };
+
+                transporter.sendMail(mailOptions, function(error, data){
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ', data);
+                    }
+                });
+            }
+
+            console.log({ data });
+            // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
+        } catch (err) {
+            console.log(err.stack);
+        }
+
+        // console.log(res.rows[0]);
+        // log({ newObj });
+
+        // const client_pg = new Client();
+        // await client_pg.connect();
+        // const res = await client_pg.query('SELECT $1::text as message', ['Hello world!']);
+        // console.log(res.rows[0].message); // Hello world!
+
+        //return new Promise((resolve) => {
+
+        // const insert = client_pg.query('insert into users values(nextval(\'users_id_seq\'), $1, $2, $3)', ['patient@transplant.' + generateToken(), generateToken(), false]);
+
+        //});
+
+        await client_pg.end();
+
+
 
         const jsonString = JSON.stringify(Object.assign({}, bodyArr))
 
